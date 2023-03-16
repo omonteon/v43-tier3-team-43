ac74310be756d1180b29d6e88d495d57a075bb5a
Ethan Lee <dalpong2014@gmail.com>
2023-03-16 14:43:41 -0400

    socket.js file made in api folder

---
+++ circle-meets/pages/api/socket.js
@@ -0,0 +1,20 @@
+// pages/api/socket.js
+import { Server } from "socket.io";
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log("Socket is already attached");
+    return res.end();
+  }
+
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
+
+  io.on("connection", (socket) => {
+    console.log(`User Connected : ${socket.id}`);
+  });
+
+  return res.end();
+};
+
+export default SocketHandler;
