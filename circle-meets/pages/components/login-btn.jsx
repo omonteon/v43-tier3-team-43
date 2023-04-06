f34335deca841381aa7171d52f95cff67a33f0e1
Ethan Lee <dalpong2014@gmail.com>
2023-04-06 09:11:11 -0400

    latest code working on signup page

--- circle-meets/pages/components/login-btn.jsx
+++ circle-meets/pages/components/login-btn.jsx
@@ -6,7 +6,7 @@ export default function Login() {
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
+        Signed in as {session.user.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
